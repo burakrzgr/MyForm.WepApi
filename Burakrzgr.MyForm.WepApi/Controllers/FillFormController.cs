7cf01cbbd8cd31e1eb208ca223a1c19a74de392c
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-20 23:59:34 +0300

    format

--- Burakrzgr.MyForm.WepApi/Controllers/FillFormController.cs
+++ Burakrzgr.MyForm.WepApi/Controllers/FillFormController.cs
@@ -8,25 +8,25 @@ namespace Burakrzgr.MyForm.WepApi.Controllers
     [Route("[controller]")]
     public class FillFormController : Controller
     {
-            private readonly ILogger<FormController> _logger;
-            private readonly IFormService _formService;
+        private readonly ILogger<FormController> _logger;
+        private readonly IFormService _formService;
 
-            public FillFormController(ILogger<FormController> logger, IFormService formService)
-            {
-                _logger = logger;
-                _formService = formService;
-            }
-
-            [HttpGet, Route("{id}")]
-            public Form Get(int id)
-            {
-                return _formService.GetForm(id);
-            }
-
-            [HttpGet, Route("")]
-            public IEnumerable<Form> GetList()
-            {
-                return _formService.GetFormList();
-            }
+        public FillFormController(ILogger<FormController> logger, IFormService formService)
+        {
+            _logger = logger;
+            _formService = formService;
         }
+
+        [HttpGet, Route("{id}")]
+        public Form Get(int id)
+        {
+            return _formService.GetForm(id);
+        }
+
+        [HttpGet, Route("")]
+        public IEnumerable<Form> GetList()
+        {
+            return _formService.GetFormList();
+        }
+    }
 }
