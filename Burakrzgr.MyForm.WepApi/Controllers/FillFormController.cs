b8b1d854fb5163fd64f86d333018d654acdbfd03
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-20 23:59:12 +0300

    fill form

---
+++ Burakrzgr.MyForm.WepApi/Controllers/FillFormController.cs
@@ -0,0 +1,32 @@
+﻿using Burakrzgr.MyForm.Business.FormTemplate;
+using Burakrzgr.MyForm.Entity.Model.FormTemplate.FormTemplate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Burakrzgr.MyForm.WepApi.Controllers
+{  
+    [ApiController]
+    [Route("[controller]")]
+    public class FillFormController : Controller
+    {
+            private readonly ILogger<FormController> _logger;
+            private readonly IFormService _formService;
+
+            public FillFormController(ILogger<FormController> logger, IFormService formService)
+            {
+                _logger = logger;
+                _formService = formService;
+            }
+
+            [HttpGet, Route("{id}")]
+            public Form Get(int id)
+            {
+                return _formService.GetForm(id);
+            }
+
+            [HttpGet, Route("")]
+            public IEnumerable<Form> GetList()
+            {
+                return _formService.GetFormList();
+            }
+        }
+}
