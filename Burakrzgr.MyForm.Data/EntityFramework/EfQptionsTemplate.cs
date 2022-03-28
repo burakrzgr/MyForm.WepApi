5137557b4957e738dd0a3c4dd6db2ab0eb76da8a
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-28 22:51:09 +0300

    options added

---
+++ Burakrzgr.MyForm.Data/EntityFramework/EfQptionsTemplate.cs
@@ -0,0 +1,31 @@
+﻿using Burakrzgr.MyForm.Core;
+using Burakrzgr.MyForm.Data.Interfaces;
+using Burakrzgr.MyForm.Entity.Entities;
+using Burakrzgr.MyForm.Entity.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burakrzgr.MyForm.Data.EntityFramework
+{
+    public class EfOptionsTemplate : IOptionsTemplate
+    {
+        FormDbContext _factory;
+        public EfOptionsTemplate(FormDbContext factory)
+        {
+            _factory = factory;
+        }
+
+        public IResult<int> AddOptions(string[] options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult<int> MergeOptions(IQuestionTemplate[] templates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Burakrzgr.MyForm.Data/Interfaces/IOptionsTemplate.cs
@@ -0,0 +1,15 @@
+﻿using Burakrzgr.MyForm.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burakrzgr.MyForm.Data.Interfaces
+{
+    public interface IOptionsTemplate
+    {
+        public IResult<int> AddOptions(string[] options);
+        public IResult<int> MergeOptions(IQuestionTemplate[] templates);
+    }
+}
