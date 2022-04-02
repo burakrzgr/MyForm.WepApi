19eaf12c4cbe97ce2dd53412c984c674277f71aa
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-02 23:47:25 +0300

    filled form entities

--- Burakrzgr.MyForm.Entity/Model/FilledForm/FilledForm.cs
+++ Burakrzgr.MyForm.Entity/Model/FilledForm/FilledForm.cs
@@ -1,6 +1,13 @@
-﻿namespace Burakrzgr.MyForm.Entity.Model.FilledForm
+﻿using Burakrzgr.MyForm.Entity.Model.FormTemplate.FormTemplate;
+
+namespace Burakrzgr.MyForm.Entity.Model.FilledForm
 {
     public class FilledForm
     {
+        public Form FormTemplate { get; set; }
+        public string User { get; set; }
+        public DateTime FillDate { get; set; }
+        public bool IsComplete { get; set; }
+        public bool PersonelInfoShared { get; set; }
     }
 }
