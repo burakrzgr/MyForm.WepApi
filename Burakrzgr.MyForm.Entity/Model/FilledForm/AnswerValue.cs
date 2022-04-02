59cda4e3a2475b766109b6bca82d2db70e537dc5
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-02 23:50:19 +0300

    asnswer added

---
+++ Burakrzgr.MyForm.Entity/Model/FilledForm/AnswerValue.cs
@@ -0,0 +1,14 @@
+﻿using Burakrzgr.MyForm.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burakrzgr.MyForm.Entity.Model.FilledForm
+{
+    public class AnswerValue
+    {
+        public QuestionTemplate questionTemplate { get; set; }
+    }
+}
