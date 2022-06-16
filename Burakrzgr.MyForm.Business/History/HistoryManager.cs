248915c64854eec0564d112a8b9a7ace833dd398
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-06-16 22:00:52 +0300

    history manager

---
+++ Burakrzgr.MyForm.Business/History/HistoryManager.cs
@@ -0,0 +1,34 @@
+﻿using Burakrzgr.MyForm.Core;
+using Burakrzgr.MyForm.Entity.Model.Auth;
+using Burakrzgr.MyForm.Entity.Model.CompletedForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burakrzgr.MyForm.Business.History
+{
+    public class HistoryManager : IHistoryService
+    {
+        public bool AddOperation(OperationModel operation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<OperationModel> GetOperationList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SuccessResult<IList<UserViewModel>> GetRedirectableUser(int formId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SuccessResult<UserViewModel> RedirectToUser(int formId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
