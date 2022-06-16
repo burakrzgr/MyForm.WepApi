ffd7b4c468e1a9b329502187ee9d78c8efc505a1
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-06-16 21:48:09 +0300

    operation

--- Burakrzgr.MyForm.WepApi/Controllers/OperationController.cs
+++ Burakrzgr.MyForm.WepApi/Controllers/OperationController.cs
@@ -2,24 +2,37 @@
 using Burakrzgr.MyForm.Entity.Model.CompletedForm;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Burakrzgr.MyForm.Core;
 
 namespace Burakrzgr.MyForm.WepApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class OperationController : ControllerBase
     {
-        IHistoryService _historyService;
-        public OperationController(IHistoryService historyService)
+        //IHistoryService _historyService;
+        public OperationController()//IHistoryService historyService
         {
            
         }
 
-        [HttpGet, Route("")]
-        public IList<OperationModel> GetList()
+        [HttpGet, Route("{formId}")]
+        public IList<OperationModel> GetList(int formId)
         {
-            var result = _historyService.GetOperationList();
+            var result = new List<OperationModel> { };// _historyService.GetOperationList();
             return result;
+        }
+
+        [HttpPost, Route("{formId}/Redirect/{userId}")]
+        public IResult<string> RedirectToUser(int formId,int userId)
+        {
+            return new SuccessResult<string>("Success!"+formId+" "+userId);
+        }
+
+        [HttpGet, Route("{formId}/Users")]
+        public IResult<IList<string>> GetRedirectableUser(int formId)
+        {
+            return new SuccessResult<IList<string>>(new List<string> { "User 1","Userr 2","User "+formId });
         }
 
     }
